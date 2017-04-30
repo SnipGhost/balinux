c8088acab23802fce68a28d2055b7d42c1e5271c
SnipGhost <snipghost@list.ru>
2017-04-30 20:29:27 +0300

    Fixed restart for servers

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,3 +8,6 @@ cp -f apache-ports.conf /etc/apache2/ports.conf
 cp -f apache-default.conf /etc/apache2/sites-enabled/000-default.conf
 cp -f index.html /var/www/html/index.html
 cp -f index.php /var/www/html/index.php
+systemctl start apache2
+systemctl restart nginx
+netstat -nlpt
