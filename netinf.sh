75ed59b3b2671b4d2ced675278f05c6c036faea8
SnipGhost <snipghost@list.ru>
2017-05-19 12:41:30 +0300

    Test new filesave system

--- netinf.sh
+++ netinf.sh
@@ -6,18 +6,24 @@ if [ -z $1 ]
 then
 	BASEDIR=`dirname $0`
 	DIRPATH=`cd $BASEDIR; pwd`
-    f=$DIRPATH/data/`date +%Y.%m.%d_%H:%M:%S_netinf`
+    f=$DIRPATH/data/curr_netinf
+    l=$DIRPATH/data/last_netinf
 else
     f="$1"
 fi
 #--------------------------------------------------------------------------------------------------
 # Print net info
 #--------------------------------------------------------------------------------------------------
+if [ -z $l ]
+	$f > $l
+fi
+#--------------------------------------------------------------------------------------------------
 printf "<table border=\"1\">\n" > $f
 (sudo cat /proc/net/dev | head -n 1 | sed 's/Inter-|/IF /g' | sed -e 's/|/_ _ _ _ _ _ _ /g'; \
  sudo cat /proc/net/dev | tail -n +2 | sed -e 's/face/\_/g' | sed -e 's/|/ /g')              \
  | column -t | sed -e 's/_/ /g' | awk \
 '{ split($0, k)
+   getline line < FILE;
    print "<tr>";
    { if (NR == 1) 
      { print "<td rowspan=\"2\">"k[1]"</td><td colspan=\"8\">"k[2]"</td><td colspan=\"8\">"k[3]"</td>"; }
--- sysinfo.php
+++ sysinfo.php
@@ -3,14 +3,15 @@
 		<title>SysInfo</title>
 		<meta HTTP-EQUIV="REFRESH" CONTENT="30">
 	</head>
-	<body>
+	<body style="background-color: black; color: white;">
 
-		<h1>Test sysinfo</h1>
+		<h1>Sysinfo</h1>
+
+		<br>
 
 		<pre>
 		<?php
 			$ini = parse_ini_file("scripts.ini");
-			echo "<br><hr><br>";
 			echo system($ini['loadavg']);
 			echo system($ini['iostat']);
 			echo system($ini['netinf']);
@@ -18,8 +19,6 @@
 			echo system($ini['netcon']);
 			echo system($ini['cpuinf']);
 			echo system($ini['diskst']);
-			echo "<br><hr><br>";
-			echo system("top -b -n 1 | head -n 15");
 		?>
 		</pre>
 
